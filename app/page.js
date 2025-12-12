6f83dfa1a4675aec9cff557c40c4b7a2989942a5
kchambers0 <kevinchambers0@gmail.com>
2025-12-12 16:44:57 -0500

    adding form stuff

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 
@@ -14,6 +15,31 @@ import 'swiper/css/navigation';
 import { Pagination, Navigation } from 'swiper/modules';
 
 export default function Home() {
+
+  const [isSent, setIsSent] = useState(false);
+  const [error, setError] = useState(false);
+  const submitBtn = useRef(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const myForm = event.target;
+      const formData = new FormData(myForm);
+      const res = await fetch('/__forms.html', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(formData).toString()
+      });
+      if (res.status === 200) {
+        setIsSent(true)
+      } else {
+        setError(`${res.status} ${res.statusText}`);
+      }
+    } catch (e) {
+      setError(`${e}`);
+    }
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -65,15 +91,36 @@ export default function Home() {
           <h2>Reach out</h2>
           <p>Have a story or something you'd like to share about David? Let us know.</p>
 
-          <form>
-            <fieldset>
-              <input type='text' id="name" name="name" placeholder="Name" />
-              <input type='email' id="email" name="email" placeholder="Email" />
-            </fieldset>
-            <textarea name="message" id="message" placeholder="Your Message"></textarea>
-            <button type="submit">Submit</button>
-          </form>
+
+          {isSent ? (
+            error ? (
+
+              <div className="message-sent">
+                <h2>Something went wrong!</h2>
+                <h3>An error has occured. Please reload the page and try again.</h3>
+                <a href="#" className="btn btn-pink" onClick={(e) => { e.preventDefault(); location.reload(); }}>Reload</a>
+              </div>
+
+            ) : (
+
+              <div className="message-sent">
+                <h2>Thank you for contacting us.</h2>
+                <p>We'll review your submission at our earliest convenience.</p>
+              </div>
+
+            )
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <input type='text' id="name" name="name" placeholder="Name" />
+                <input type='email' id="email" name="email" placeholder="Email" />
+              </fieldset>
+              <textarea name="message" id="message" placeholder="Your Message"></textarea>
+              <button type="submit">Submit</button>
+            </form>
+          )}
         </div>
+
 
         <footer className={styles.footer}>
           <p>In Memory of David Sutalo | October 2, 1952 - December 3, 2025</p>
